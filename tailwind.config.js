8945dedf3c90412b45e177215dbf4af3dcc470e0
sharif.ahmed <sharif@commlinkinfotech.com.bd>
2023-07-11 22:36:54 +0600

    un necessary data removed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,6 @@
 module.exports = {
   content: ['*'],
   theme: {
-    fontFamily:{
-      lato : ['Lato','Georgia',],
-      open_sans:['"Open Sans"','Georgia',],
-    },
-    fontSize: {
-      custom_font_size:['1.75rem']
-    },
-    fontWeight: {
-      custom_font_weight:['800']
-    },
-    letterSpacing: {
-      custom_latter_space:['28px']
-    },
     extend: {},
   },
   plugins: [],
